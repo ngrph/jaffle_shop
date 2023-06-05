4ae2a882b52536454f48d09b09364a67f5da4ffb
serge <smosesov@gmail.com>
2023-06-05 11:26:22 +0100

    Modified model.jaffle_shop.payment_revenue_2018.

--- models/payment_revenue_2018.sql
+++ models/payment_revenue_2018.sql
@@ -1,28 +1,33 @@
-with source as (
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+orders as (
+    select * from {{ ref('stg_orders') }}
+),
+
+joined_data as (
     select
         p.payment_method,
         p.amount,
-        o.order_date as created_at
-    from {{ ref('stg_payments') }} as p
-    join {{ ref('stg_orders') }} as o
-    on p.order_id = o.order_id
+        o.order_date as payment_date
+    from payments p
+    join orders o on p.order_id = o.order_id
 ),
 
-filtered_2018 as (
+filtered_payments as (
     select
         payment_method,
-        amount,
-        date_trunc('year', created_at) as year
-    from source
-    where date_trunc('year', created_at) = '2018-01-01'
-),
-
-aggregated as (
-    select
-        payment_method,
-        sum(amount) as total_revenue
-    from filtered_2018
+        sum(amount) as revenue
+    from joined_data
+    where extract(year from payment_date) = 2018
     group by payment_method
 )
 
-select * from aggregated
+select * from filtered_payments
