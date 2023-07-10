107ed502b734719d3427d049d4ab493ce7dc27fc
Serge Mosesov <smosesov@gmail.com>
2023-07-10 10:15:05 +0100

    fix staging models to work with main models in original repo

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -3,13 +3,15 @@
         select * from {{source('jaffle_shop_raw', 'customers')}}
     ),
 
-    renamed as (
-        select
-            id,
+renamed as (
+
+    select
+        id as customer_id,
         first_name,
         last_name
-        from source
-    )
 
-    select * from renamed
-    
+    from source
+
+)
+
+select * from renamed
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,15 +3,16 @@
         select * from {{source('jaffle_shop_raw', 'orders')}}
     ),
 
-    renamed as (
-        select
-            id,
-        user_id,
-        order_date,
-        status,
-        _etl_loaded_at
-        from source
-    )
+renamed as (
 
-    select * from renamed
-    
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from source
+
+)
+
+select * from renamed
